03b79d275fc81e47d454410df35d95e8a2b43da7
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-10-13 23:30:11 +0900

    Project Skeleton Foundation Desgin

---
+++ README.md
@@ -0,0 +1,9 @@
+## go chating app
+
+### start app
+
+```
+go run cmd/web/*.go
+```
+
+go to http://localhost:8080
---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"chat/internal/handlers"
+	"net/http"
+
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+
+	return mux
+}
