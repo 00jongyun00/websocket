5171c66551a07a9fd0cf07037a8751296e00483b
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-10-17 15:48:47 +0900

    Sending messages

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -117,7 +117,12 @@ func ListenToWsChannel() {
 			users := getUserList()
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
+			broadcastToAll(response)
 		}
+
 		// response.Action = "Got here"
 		// response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
 		// broadcastToAll(response)
